7165b75d1f7aba8f44a92d22862c0d822e4f06f7
KaimuSeino <skim07a19yki@icloud.com>
2024-09-04 19:08:39 +0900

    LessonWordモデルとLessonWordResposeを追加

---
+++ model/lesson_word.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type LessonWord struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Lesson    Lesson    `json:"lesson" gorm:"foreignKey:LessonId; constraint:OnDelete:CASCADE"`
+	LessonID  uint      `json:"lesson_id" gorm:"not null"`
+	WordID    uint      `json:"word_id" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type LessonWordResponse struct {
+	ID       uint `json:"id" gorm:"primaryKey"`
+	LessonID uint `json:"lesson_id"`
+	WordID   uint `json:"word_id"`
+}
