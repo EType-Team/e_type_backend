d350d276f0551cc3b09999a15068b4cda91d5d14
KaimuSeino <skim07a19yki@icloud.com>
2024-09-04 19:18:33 +0900

    UserWordProgressモデルとUserWordProgressResponseを追加

---
+++ model/user_word_progress.go
@@ -0,0 +1,23 @@
+package model
+
+import "time"
+
+type UserWordProgress struct {
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	UserID       uint      `json:"user_id" gorm:"not null"`
+	WordID       uint      `json:"word_id" gorm:"not null"`
+	TotalTypings int       `json:"total_typings"`
+	TypingSpeed  float64   `json:"typing_speed"`
+	Proficiency  float64   `json:"proficiency"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type UserWordProgressResponse struct {
+	ID           uint    `json:"id" gorm:"primaryKey"`
+	UserID       uint    `json:"user_id" gorm:"not null"`
+	WordID       uint    `json:"word_id" gorm:"not null"`
+	TotalTypings int     `json:"total_typings"`
+	TypingSpeed  float64 `json:"typing_speed"`
+	Proficiency  float64 `json:"proficiency"`
+}
