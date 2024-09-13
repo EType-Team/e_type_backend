9b11dee3abd730d29315d3f7c6ce4e57af990161
KaimuSeino <skim07a19yki@icloud.com>
2024-09-13 20:39:01 +0900

    UserWordProgressにWordフィールドを追加し、UserWordProgressResponseに対応するフィールドを追加。

--- model/user_word_progress.go
+++ model/user_word_progress.go
@@ -6,6 +6,7 @@ type UserWordProgress struct {
 	ID           uint      `json:"id" gorm:"primaryKey"`
 	UserID       uint      `json:"user_id" gorm:"not null"`
 	WordID       uint      `json:"word_id" gorm:"not null"`
+	Word         Word      `json:"word" gorm:"foreignKey:WordID"`
 	TotalTypings int       `json:"total_typings"`
 	TypingSpeed  float64   `json:"typing_speed"`
 	Proficiency  float64   `json:"proficiency"`
@@ -17,6 +18,7 @@ type UserWordProgressResponse struct {
 	ID           uint    `json:"id" gorm:"primaryKey"`
 	UserID       uint    `json:"user_id" gorm:"not null"`
 	WordID       uint    `json:"word_id" gorm:"not null"`
+	Word         Word    `json:"word"`
 	TotalTypings int     `json:"total_typings"`
 	TypingSpeed  float64 `json:"typing_speed"`
 	Proficiency  float64 `json:"proficiency"`
