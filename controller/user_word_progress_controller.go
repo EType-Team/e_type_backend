82108a1e46dbf083f0fb58e080d5cdb337b10c12
KaimuSeino <skim07a19yki@icloud.com>
2024-09-14 16:19:39 +0900

    不要なメソッドの削除と更新

--- controller/user_word_progress_controller.go
+++ controller/user_word_progress_controller.go
@@ -12,10 +12,8 @@ import (
 
 type IUserWordProgressController interface {
 	GetAllUserWordProgress(c echo.Context) error
-	GetUserWordProgressById(c echo.Context) error
-	CreateUserWordProgress(c echo.Context) error
-	UpdateUserWordProgress(c echo.Context) error
-	IncrementUserWordProgress(c echo.Context) error
+	IncrementOrCreateUserWordProgress(c echo.Context) error
+	GetUserWordProgressByWordId(c echo.Context) error
 }
 
 type userWordProgressController struct {
@@ -38,58 +36,7 @@ func (uwpc *userWordProgressController) GetAllUserWordProgress(c echo.Context) e
 	return c.JSON(http.StatusOK, userWordProgressRes)
 }
 
-func (uwpc *userWordProgressController) GetUserWordProgressById(c echo.Context) error {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
-	userId := claims["user_id"]
-	id := c.Param("userWordProgressId")
-	userWordProgressId, _ := strconv.Atoi(id)
-
-	userWordProgressRes, err := uwpc.uwpu.GetUserWordProgressById(uint(userId.(float64)), uint(userWordProgressId))
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusOK, userWordProgressRes)
-}
-
-func (uwpc *userWordProgressController) CreateUserWordProgress(c echo.Context) error {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
-	userId := claims["user_id"]
-
-	userWordProgress := model.UserWordProgress{}
-	if err := c.Bind(&userWordProgress); err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	userWordProgress.UserID = uint(userId.(float64))
-	userWordProgressRes, err := uwpc.uwpu.CreateUserWordProgress(userWordProgress)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusOK, userWordProgressRes)
-}
-
-func (uwpc *userWordProgressController) UpdateUserWordProgress(c echo.Context) error {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
-	userId := claims["user_id"]
-	id := c.Param("userWordProgressId")
-	userWordProgressId, _ := strconv.Atoi(id)
-
-	userWordProgress := model.UserWordProgress{}
-	if err := c.Bind(&userWordProgress); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-
-	userWordProgressRes, err := uwpc.uwpu.UpdateUserWordProgress(userWordProgress, uint(userId.(float64)), uint(userWordProgressId))
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-
-	return c.JSON(http.StatusOK, userWordProgressRes)
-}
-
-func (uwpc *userWordProgressController) IncrementUserWordProgress(c echo.Context) error {
+func (uwpc *userWordProgressController) IncrementOrCreateUserWordProgress(c echo.Context) error {
 	user := c.Get("user").(*jwt.Token)
 	claims := user.Claims.(jwt.MapClaims)
 	userId := claims["user_id"]
@@ -99,9 +46,27 @@ func (uwpc *userWordProgressController) IncrementUserWordProgress(c echo.Context
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 
-	userWordProgressRes, err := uwpc.uwpu.IncrementTotalTypings(uint(userId.(float64)), userWordProgress.WordID)
+	userWordProgressRes, err := uwpc.uwpu.IncrementOrCreateUserWordProgress(userWordProgress, uint(userId.(float64)), userWordProgress.WordID)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, userWordProgressRes)
+}
+
+func (uwpc *userWordProgressController) GetUserWordProgressByWordId(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	wordId, err := strconv.Atoi(c.Param("wordId"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid word ID")
+	}
+
+	userWordProgress, err := uwpc.uwpu.GetUserWordProgressByWordId(uint(userId.(float64)), uint(wordId))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, userWordProgress)
 }
