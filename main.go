890d32b7e30a08387fad33dfe86e44dcfe6d1670
KaimuSeino <skim07a19yki@icloud.com>
2024-09-15 14:56:02 +0900

    環境変数のロード処理をmainで実行し、oauth関連の環境変数をロードする

--- main.go
+++ main.go
@@ -7,15 +7,38 @@ import (
 	"api/router"
 	"api/usecase"
 	"api/validator"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	oauthConfig := &oauth2.Config{
+		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URI"),
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"openid",
+		},
+		Endpoint: google.Endpoint,
+	}
+
 	db := db.NewDB()
 
 	userValidator := validator.NewUserValidator()
 	userRepository := repository.NewUserRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
-	userController := controller.NewUserController(userUsecase)
+	userController := controller.NewUserController(userUsecase, oauthConfig)
 
 	lessonRepository := repository.NewLessonRepository(db)
 	lessonUsecase := usecase.NewLessonUsecase(lessonRepository)
