bc7bfbad71c405b28ddbd6a99bc9dc8907aec112
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 16:13:18 +0900

    migrateファイルを編集

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -4,9 +4,16 @@ import (
 	"api/db"
 	"api/model"
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	dbConn := db.NewDB()
 	defer fmt.Println("Successfully Migrated")
 	defer db.CloseDB(dbConn)
