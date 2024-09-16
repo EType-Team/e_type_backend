cb2c3105c95bac6853675073689227291affc63a
KaimuSeino <skim07a19yki@icloud.com>
2024-09-16 16:13:29 +0900

    usermodelの変更

--- model/user.go
+++ model/user.go
@@ -3,14 +3,13 @@ package model
 import "time"
 
 type User struct {
-	ID            uint      `json:"id" gorm:"primaryKey"`
-	Name          string    `json:"name"`
-	Email         string    `json:"email" gorm:"unique"`
-	Password      string    `json:"password"`
-	EmailVerified time.Time `json:"email_verified"`
-	Image         string    `json:"image"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	Image     string    `json:"image"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
@@ -18,4 +17,8 @@ type UserResponse struct {
 	Name  string `json:"name"`
 	Email string `json:"email" gorm:"unique"`
 	Image string `json:"image"`
+}
+
+type UpdateUserRequest struct {
+	Name string `json:"name"`
 }
