967538ecc6904be8dc81ce47c63a0c181f48bdc6
kokoro <lily.koko412@gmail.com>
2024-09-30 23:27:25 +0900

    feat: CreateLesson ユースケースを実装

--- usecase/lesson_usecase.go
+++ usecase/lesson_usecase.go
@@ -8,14 +8,16 @@ import (
 type ILessonUsecase interface {
 	GetAllLesson() ([]model.LessonResponse, error)
 	GetLessonById(lessonId uint) (model.LessonResponse, error)
+	CreateLesson(lesson *model.Lesson, words []model.Word) error
 }
 
 type lessonUsecase struct {
 	lr repository.ILessonRepository
+	wr repository.IWordRepository
 }
 
-func NewLessonUsecase(lr repository.ILessonRepository) ILessonUsecase {
-	return &lessonUsecase{lr}
+func NewLessonUsecase(lr repository.ILessonRepository, wr repository.IWordRepository) ILessonUsecase {
+	return &lessonUsecase{lr, wr}
 }
 
 func (lu *lessonUsecase) GetAllLesson() ([]model.LessonResponse, error) {
@@ -46,4 +48,27 @@ func (lu *lessonUsecase) GetLessonById(lessonId uint) (model.LessonResponse, err
 		Description: lesson.Description,
 	}
 	return resLesson, nil
+}
+
+
+func (lu *lessonUsecase) CreateLesson(lesson *model.Lesson, words []model.Word) error {
+    if err := lu.lr.CreateLesson(lesson); err != nil {
+        return err
+    }
+
+    for _, word := range words {
+        if err := lu.wr.CreateWord(&word); err != nil {
+            return err
+        }
+        lessonWord := model.LessonWord{
+            LessonID: lesson.ID,
+            WordID:   word.ID,
+        }
+
+        if err := lu.lr.CreateLessonWord(&lessonWord); err != nil {
+            return err
+        }
+    }
+
+    return nil
 }
